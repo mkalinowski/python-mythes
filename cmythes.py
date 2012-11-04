2dbdaca93d9f05ded90332428195cf25b0774480
Mikolaj Kalinowski <mikolaj.kalinowski@gmail.com>
2012-11-04 13:47:03 +0100

    Add lookup method

--- cmythes.py
+++ cmythes.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
-from ctypes import cdll, c_char_p
+from ctypes import byref, cdll, c_char_p, c_int, POINTER, Structure
 import sys
 
 
 class MyThes(object):
 
     lib = cdll.LoadLibrary('./libcmythes.so')
+
+    class mentry(Structure):
+        # memory representation of a transparent lib struct
+        _fields_ = [('defn', c_char_p),
+                    ('count', c_int),
+                    ('phyns', POINTER(c_char_p))]
 
     def __init__(self, idxpath, datpath):
 
@@ -26,10 +32,27 @@ class MyThes(object):
         fun.restype = c_char_p
         return fun(self.obj).decode('latin1')
 
+    def lookup(self, text):
+        if isinstance(text, str):
+            text = text.encode('utf-8', 'surrogateescape')
+
+        # mentry pointer. initialized to NULL
+        mentry_p = POINTER(self.mentry)() 
+
+        mentries = []
+        count = self.lib.MyThes_Lookup(self.obj, c_char_p(text),
+                                       c_int(len(text)), byref(mentry_p))
+        for i in range(count):
+            mentry = mentry_p[i]
+            print(mentry.defn)
+
+        self.lib.MyThes_CleanUpAfterLookup(self.obj, byref(mentry_p), count)
+        return mentries
+
 if __name__ == "__main__":
     idxpath = "/usr/share/mythes/th_en_US_v2.idx"
     datpath = "/usr/share/mythes/th_en_US_v2.dat"
 
     thesaurus = MyThes(idxpath, datpath)
     encoding = thesaurus.get_th_encoding()
-    print (encoding)
+    thesaurus.lookup("accost")
