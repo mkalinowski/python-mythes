d50a5f67cac7f5e50342713ac79d2f09c08a9a98
Mikolaj Kalinowski <mikolaj.kalinowski@gmail.com>
2012-11-04 01:13:24 +0100

    Add cmythes - C to C++ glue
    Python's ctypes works only with plain C, hence this library.

---
+++ cmythes.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <mythes.hxx>
+extern "C" {
+
+    MyThes* MyThes_new(const char* idxpath, const char* datpath)
+    {
+        return new MyThes(idxpath, datpath);
+    }
+
+    void MyThes_del(MyThes* instance)
+    {
+        delete instance;
+    }
+
+    int MyThes_Lookup(MyThes* instance, const char * pText, int len,
+                      mentry** pme)
+    {
+        return instance->Lookup(pText, len, pme);
+    }
+
+    void MyThes_CleanUpAfterLookup(MyThes* instance, mentry** pme, int nmean)
+    {
+        instance->CleanUpAfterLookup(pme, nmean);
+    }
+
+    char *MyThes_get_th_encoding(MyThes* instance)
+    {
+        return instance->get_th_encoding();
+    }
+}
---
+++ cmythes.h
@@ -0,0 +1,12 @@
+typedef struct MyThes MyThes;
+typedef struct mentry mentry;
+
+MyThes* MyThes_new(const char* idxpath, const char* datpath);
+
+void MyThes_del(MyThes* instance);
+
+int MyThes_Lookup(MyThes* instance, const char* pText, int len, mentry** pme);
+
+void MyThes_CleanUpAfterLookup(MyThes* instance, mentry** pme, int nmean);
+
+char* MyThes_get_th_encoding(MyThes* instance);
