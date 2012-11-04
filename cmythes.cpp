dc3e84ed85c126b743506a2031a0776992d4fc36
Mikolaj Kalinowski <mikolaj.kalinowski@gmail.com>
2012-11-04 13:05:14 +0100

    Fix formatting

--- cmythes.cpp
+++ cmythes.cpp
@@ -2,28 +2,28 @@
 #include <mythes.hxx>
 extern "C" {
 
-    MyThes* MyThes_new(const char* idxpath, const char* datpath)
+    MyThes *MyThes_new(const char *idxpath, const char *datpath)
     {
         return new MyThes(idxpath, datpath);
     }
 
-    void MyThes_del(MyThes* instance)
+    void MyThes_del(MyThes *instance)
     {
         delete instance;
     }
 
-    int MyThes_Lookup(MyThes* instance, const char * pText, int len,
-                      mentry** pme)
+    int MyThes_Lookup(MyThes *instance, const char *pText, int len,
+                      mentry **pme)
     {
         return instance->Lookup(pText, len, pme);
     }
 
-    void MyThes_CleanUpAfterLookup(MyThes* instance, mentry** pme, int nmean)
+    void MyThes_CleanUpAfterLookup(MyThes *instance, mentry **pme, int nmean)
     {
         instance->CleanUpAfterLookup(pme, nmean);
     }
 
-    char *MyThes_get_th_encoding(MyThes* instance)
+    char *MyThes_get_th_encoding(MyThes *instance)
     {
         return instance->get_th_encoding();
     }
