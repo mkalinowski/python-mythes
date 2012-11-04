f26cd8ba9c7e80ddebbdb1accd286bd5b550f9ae
Mikolaj Kalinowski <mikolaj.kalinowski@gmail.com>
2012-11-04 13:23:47 +0100

    Python 3 utf-8 compatibility. Implement first FFI methods.

--- cmythes.py
+++ cmythes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 from ctypes import cdll, c_char_p
 import sys
@@ -10,13 +10,26 @@ class MyThes(object):
 
     def __init__(self, idxpath, datpath):
 
+        # C functions require ASCII, but Python 3 strings are UTF-8
+        if isinstance(idxpath, str):
+            idxpath = idxpath.encode('utf-8', 'surrogateescape')
+        if isinstance(datpath, str):
+            datpath = datpath.encode('utf-8', 'surrogateescape')
+
         self.obj = self.lib.MyThes_new(c_char_p(idxpath), c_char_p(datpath))
 
     def __del__(self):
         self.lib.MyThes_del(self.obj)
 
-if __name__ == "__main__":
-    idxpath = b"/usr/share/mythes/th_en_US_v2.idx"
-    datpath = b"/usr/share/mythes/th_en_US_v2.dat"
+    def get_th_encoding(self):
+        fun = self.lib.MyThes_get_th_encoding
+        fun.restype = c_char_p
+        return fun(self.obj).decode('latin1')
 
-    thes = MyThes(idxpath, datpath)
+if __name__ == "__main__":
+    idxpath = "/usr/share/mythes/th_en_US_v2.idx"
+    datpath = "/usr/share/mythes/th_en_US_v2.dat"
+
+    thesaurus = MyThes(idxpath, datpath)
+    encoding = thesaurus.get_th_encoding()
+    print (encoding)
